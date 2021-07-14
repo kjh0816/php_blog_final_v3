040f78afcc476a589b9ce2e0d3a4bddb37c7000e
kjh0816 <readshot2@gmail.com>
2021-07-14 12:35:14 +0900

    상세 페이지 컨테이너 적용(페이지에서 가운데에 나타나도록)

--- src/view/usr/article/detail.php
+++ src/view/usr/article/detail.php
@@ -17,70 +17,71 @@ $utterancPageIdentifier = "/usr/article/detail?id={$article['id']}";
 
 
 <section class="section-article-menu flex mt-2">
-  <div class="container mx-auto flex">
-    <a href="list" class="btn btn-link">리스트</a>
-    <a href="modify?id=<?=$article['id']?>" class="btn btn-link">수정</a>
-    <a class="btn btn-link" onclick="if ( confirm('정말 삭제 하시겠습니까?') == false ) return false;" href="doDelete?id=<?=$article['id']?>">삭제</a>
-  </div>
-  
-</section>
-<hr class="ml-4">
-<section class="section-article-menu">
-  <div class="container mx-auto ml-4">
+<div class="container mx-auto flex">
+  <a href="list" class="btn btn-link">리스트</a>
+  <a href="modify?id=<?=$article['id']?>" class="btn btn-link">수정</a>
+  <a class="btn btn-link" onclick="if ( confirm('정말 삭제 하시겠습니까?') == false ) return false;" href="doDelete?id=<?=$article['id']?>">삭제</a>
+</div>
 
-  <div class="py-5">
-            <?php
-            $detailUri = "detail?id=${article['id']}";
-            $body = ToastUiEditor__getSafeSource($article['body']);
-            ?>
-            <div>
-              <div class="badge badge-primary badge-outline">번호</div>
-              <?=$article['id']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">제목</div>
-              <?=$article['title']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">카테고리</div>
-              <?=$article['boardName']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">작성자</div>
-              <?=$article['nickname']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">작성날짜</div>
-              <?=$article['regDate']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">수정날짜</div>
-              <?=$article['updateDate']?>
-            </div>
-            <div class="mt-2">
-              <script type="text/x-template"><?=$body?></script>
-              <div class="toast-ui-viewer"></div>
-            </div>
+</section>
+
+<section class="section-article-menu">
+<div class="container mx-auto">
+<hr class="ml-4">
+<div class="py-5">
+          <?php
+          $detailUri = "detail?id=${article['id']}";
+          $body = ToastUiEditor__getSafeSource($article['body']);
+          ?>
+          <div>
+            <div class="badge badge-primary badge-outline">번호</div>
+            <?=$article['id']?>
           </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">제목</div>
+            <?=$article['title']?>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">카테고리</div>
+            <?=$article['boardName']?>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">작성자</div>
+            <?=$article['nickname']?>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">작성날짜</div>
+            <?=$article['regDate']?>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">수정날짜</div>
+            <?=$article['updateDate']?>
+          </div>
+          <div class="mt-2">
+            <script type="text/x-template"><?=$body?></script>
+            <div class="toast-ui-viewer"></div>
+          </div>
+        </div>
+</div>
 </section>
 
 
 <section class="section-disqus">
-  <div class="container mx-auto">
-    <div class="con-pad">
-      <style>
-      .utterances {
-        max-width: 100%;
-      }
-      </style>
-      <script src="https://utteranc.es/client.js"
-        repo="kjh0816/b_jihoo_kr_comment"
-        issue-term="<?=$utterancPageIdentifier?>"
-        theme="github-dark"
-        crossorigin="anonymous"
-        async>
-      </script>
-    </div>
+<div class="container mx-auto">
+  <div class="con-pad">
+    <style>
+    .utterances {
+      max-width: 100%;
+    }
+    </style>
+    <script src="https://utteranc.es/client.js"
+      repo="kjh0816/b_jihoo_kr_comment"
+      issue-term="<?=$utterancPageIdentifier?>"
+      theme="github-dark"
+      crossorigin="anonymous"
+      async>
+    </script>
   </div>
+</div>
 </section>
 <?php require_once __DIR__ . "/../foot.php"; ?>
--- src/view/usr/article/list.php
+++ src/view/usr/article/list.php
@@ -7,26 +7,26 @@ $pageTitleIcon = '<i class="fas fa-list"></i>';
 
 <!-- 사용자가 게시판 선택 (시작) -->
 <section class="section-article-menu mt-5 ml-10">
-  <div class="container mx-auto">
-  <span class="mr-4 badge badge-primary badge-outline">카테고리 </span>
+<div class="container mx-auto">
+<span class="mr-4 badge badge-primary badge-outline">카테고리 </span>
 <select class="article-list-choose-board btn-outline btn-primary" name="boardId">
 <option value="0" class="text-pink-500">카테고리 선택</option>
 <?php foreach($boards as $board){?>
-    <option value="<?=$board['id']?>"><?=$board['name']?></option>
+  <option value="<?=$board['id']?>"><?=$board['name']?></option>
 <?php }?>
 </select>
-  <form action="list" method="POST" class="flex mt-3">
-  <div>
+<form action="list" method="POST" class="flex mt-3">
+<div>
 <span class="badge badge-primary badge-outline">
 검색어
 </span> 
-  <input type="text" placeholder="검색어를 입력해주세요." type="search" name="searchKeyword" class="input input-primary input-bordered ml-3">
+<input type="text" placeholder="검색어를 입력해주세요." type="search" name="searchKeyword" class="input input-primary input-bordered ml-3">
 </div>
 <div class="">
 <input type="submit" value="검색" class="ml-2 btn btn-outline btn-primary ">
 </div>
 </form>
-  </div>
+</div>
 </section>
 
 
@@ -38,114 +38,114 @@ $pageTitleIcon = '<i class="fas fa-list"></i>';
 <!-- 사용자가 게시판 선택 (끝) -->
 
 <?php if ( $isLogined ) { ?>
-  
+
 
 <section class="section-article-menu mx-4">
-  <hr>
-  <div class="container mx-auto">
-    <a href="write" class="btn btn-link">
-    <i class="fas fa-pen mr-2"> 글 작성</i>
-    </a>
-  </div>
-  <hr>
+<div class="container mx-auto">
+<hr>
+  <a href="write" class="btn btn-link">
+  <i class="fas fa-pen mr-2"> 글 작성</i>
+  </a>
+<hr>
+</div>
 </section>
 
 
 <?php } ?>
 
 <section class="section-articles mt-4">
-  <div class="container mx-auto">
-    <div class="con-pad">
+<div class="container mx-auto">
+  <div class="con-pad">
 
-      <div>
-        <div class="badge badge-primary badge-outline">게시물 수</div>
-        <?=$totalCount?>
-      </div>
+    <div>
+      <div class="badge badge-primary badge-outline">게시물 수</div>
+      <?=$totalCount?>
+    </div>
 
-      <hr class="mt-4">
+    <hr class="mt-4">
 
-      <div>
-        <?php foreach ( $articles as $article ) { ?>
-          <div class="py-5">
-            <?php
-            $detailUri = "detail?id=${article['id']}";
-            $body = ToastUiEditor__getSafeSource($article['body']);
-            ?>
-            <div>
-              <div class="badge badge-primary badge-outline">번호</div>
-              <a class="hover:underline" href="<?=$detailUri?>"><?=$article['id']?></a>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">제목</div>
-              <a class="hover:underline" href="<?=$detailUri?>"><?=$article['title']?></a>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">작성자</div>
-              <?=$article['nickname']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">작성날짜</div>
-              <?=$article['regDate']?>
-            </div>
-            <div class="mt-2">
-              <div class="badge badge-primary badge-outline">수정날짜</div>
-              <?=$article['updateDate']?>
-            </div>
-            <div class="mt-2">
-              <script type="text/x-template"><?=$body?></script>
-              <div class="toast-ui-viewer"></div>
-            </div>
+    <div>
+      <?php foreach ( $articles as $article ) { ?>
+        <div class="py-5">
+          <?php
+          $detailUri = "detail?id=${article['id']}";
+          $body = ToastUiEditor__getSafeSource($article['body']);
+          ?>
+          <div>
+            <div class="badge badge-primary badge-outline">번호</div>
+            <a class="hover:underline" href="<?=$detailUri?>"><?=$article['id']?></a>
           </div>
-          <hr>
-        <?php } ?>
-      </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">제목</div>
+            <a class="hover:underline" href="<?=$detailUri?>"><?=$article['title']?></a>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">작성자</div>
+            <?=$article['nickname']?>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">작성날짜</div>
+            <?=$article['regDate']?>
+          </div>
+          <div class="mt-2">
+            <div class="badge badge-primary badge-outline">수정날짜</div>
+            <?=$article['updateDate']?>
+          </div>
+          <div class="mt-2">
+            <script type="text/x-template"><?=$body?></script>
+            <div class="toast-ui-viewer"></div>
+          </div>
+        </div>
+        <hr>
+      <?php } ?>
     </div>
   </div>
+</div>
 </section>
 
 <!-- 페이징 부분 (시작)-->
 <nav  class="page-items-cover">
-                    <ul  class="page-items flex">
-                        <?php
-                            if ($page <= 1){
-                                // 빈 값
-                            } else {
-                                echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=1&boardId=$boardId&searchKeyword=$searchKeyword' aria-label='Previous'><b style='color:blue;'>처음</b></a></li>";
-                            }
-                            
-                            if ($page <= 1){
-                                // 빈 값
-                            } else {
-                                $pre = $page - 1;
-                                echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=$pre&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'>◀이전</b></a></li>";
-                            }
-                            
-                            for($i = $blockStart; $i <= $blockEnd; $i++){
-                                if($page == $i){
-                                    echo "<li class='page-item cell-left'><a disabled><b style='color: black; font-size:20px;'> $i </b></a></li>";
-                                } else {
-                                    echo "<li class='page-item cell-left'><a href='/usr/article/list?page=$i&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'> ($i) </b></a></li>";
-                                }
-                            }
-                            
-                            if($page >= $totalPage){
-                                // 빈 값
-                            } else {
-                                $next = $page + 1;
-                                echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=$next&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'> 다음▶</b></a></li>";
-                            }
-                            
-                            if($page >= $totalPage){
-                                // 빈 값
-                            } else {
-                                echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=$totalPage&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'>마지막</b></a>";
-                            }
-                        ?>                                        
-                    </ul>                                                                  
-                </nav>               
-            </div>                                            
-        </div>                                                                    
-    </div>
+                  <ul  class="page-items flex">
+                      <?php
+                          if ($page <= 1){
+                              // 빈 값
+                          } else {
+                              echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=1&boardId=$boardId&searchKeyword=$searchKeyword' aria-label='Previous'><b style='color:blue;'>처음</b></a></li>";
+                          }
+                          
+                          if ($page <= 1){
+                              // 빈 값
+                          } else {
+                              $pre = $page - 1;
+                              echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=$pre&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'>◀이전</b></a></li>";
+                          }
+                          
+                          for($i = $blockStart; $i <= $blockEnd; $i++){
+                              if($page == $i){
+                                  echo "<li class='page-item cell-left'><a disabled><b style='color: black; font-size:20px;'> $i </b></a></li>";
+                              } else {
+                                  echo "<li class='page-item cell-left'><a href='/usr/article/list?page=$i&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'> ($i) </b></a></li>";
+                              }
+                          }
+                          
+                          if($page >= $totalPage){
+                              // 빈 값
+                          } else {
+                              $next = $page + 1;
+                              echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=$next&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'> 다음▶</b></a></li>";
+                          }
+                          
+                          if($page >= $totalPage){
+                              // 빈 값
+                          } else {
+                              echo "<li class='page-item cell-left'><a class='page-link' href='/usr/article/list?page=$totalPage&boardId=$boardId&searchKeyword=$searchKeyword'><b style='color:blue;'>마지막</b></a>";
+                          }
+                      ?>                                        
+                  </ul>                                                                  
+              </nav>               
+          </div>                                            
+      </div>                                                                    
+  </div>
 
 <!-- 페이징 부분 (끝)-->
 
